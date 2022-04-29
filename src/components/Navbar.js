81240d95ff6effce64a2a644c0b9fa131d151f7c
Cristian Montes <cmontesmujica@gmail.com>
2022-04-28 21:21:00 -0600

    added component Navbar and some basic styling for it

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <div className='container'>
+            <h2> React Blog</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
