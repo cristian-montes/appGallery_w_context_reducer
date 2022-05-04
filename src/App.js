9a7f7f2c672d6d9d976c73682d75fbd0990786de
Cristian Montes <cmontesmujica@gmail.com>
2022-05-03 21:19:52 -0600

    added the Home component to group the hero and postlist for Routing porpuses, also added routing in App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import BlogProvider from './context/BlogProvider';
 import PostList from './components/PostList';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './views/Home';
 
 function App() {
   return (
     <div className="App">
       <BlogProvider>
-        <div className='App'>
-          <Navbar />
-          <Hero />
-          <PostList />
-        </div>
+          <div className='App'>
+        <Router>
+            <Navbar />
+
+          <Routes>
+            <Route path ='/' exact element={<Home />} />
+          
+          </Routes>
+          
+        </Router>
+          </div>
       </BlogProvider>
     </div>
   );
